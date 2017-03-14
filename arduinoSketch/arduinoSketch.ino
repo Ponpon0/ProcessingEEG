123c85dafe05e3450fac00c2707c9aa6ee678853
Ponpon0 <marie.pontais@gmail.com>
2017-03-14 18:15:11 -0400

    second version of arduino file

--- arduinoSketch/arduinoSketch.ino
+++ arduinoSketch/arduinoSketch.ino
@@ -1,32 +1,63 @@
-char val = 3; // Data received from the serial port
+ int incomingVal[5] = {0}; // Data received from the serial port
+
+ 
  int ledPin = 13; // Set the pin to digital I/O 13
+ int msgID = 0; 
+ int msgState = 0;
+ int msgType;
+ int msg;
+ boolean inSink = false;
  
 void setup() {
   pinMode(ledPin, OUTPUT); // Set pin as OUTPUT
    Serial.begin(9600); // Start serial communication at 9600 bps
-   if (Serial) {
+   if (!Serial) {
     Serial.print("Connecting");
   }   
+
+    // put your main code here, to run repeatedly:
+  //while (!Serial.available());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (!Serial.available());
-  val = 3;
+  
+}
+
+void serialEvent() {
+
+int cnt = 0;
+
   while (Serial.available()) { // If data is sent,
-     val = Serial.read(); // read it and store it in val
-     Serial.print(val);
-     delay(1);
+     delay(5);
+     incomingVal[cnt++] = Serial.read(); // read it and store it in val
    }
 
-   if (val == '1') 
+//   for (int i = 0; i < cnt; i++) {  //checking values received
+//    Serial.print(incomingVal[i]);
+//   }
+   
+   if (incomingVal[0] == '$') {
+
+     if (msgID == (int)incomingVal[1] ) {
+      inSink = true; 
+        msgID++;
+      }
+      msgType = incomingVal[2];
+      msg = incomingVal[3]; //stores it
+   }
+   checkLED();
+}
+
+void checkLED() {
+  if (msgType == '0') { //LED message
+   if (msg == '1') 
    { // If 1 was received
      digitalWrite(ledPin, HIGH); // turn the LED on
      //delay(100);
      //digitalWrite(ledPin, LOW);
-   } else if (val == '0') {
+   } else if (msg == '0') {
      digitalWrite(ledPin, LOW); // otherwise turn it off
-   } else if (val == '2') {
+   } else if (msg == '2') {
       digitalWrite(ledPin, HIGH); // turn the LED on
       delay(500);
       digitalWrite(ledPin, LOW);
@@ -34,4 +65,6 @@ void loop() {
       digitalWrite(ledPin, HIGH); // turn the LED on
    }
    delay(10); // Wait 10 milliseconds for next reading
+  }
 }
+
